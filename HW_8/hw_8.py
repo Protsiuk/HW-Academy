b28a8b26ab48b4d941db68fa9c1c636727a8f722
Dmitriy Protsiuk <d.Protsiuk@gmail.com>
2017-03-21 18:44:28 +0200

    Add corrected file without task of pagintor desigion end bonus

--- HW_8/hw_8.py
+++ HW_8/hw_8.py
@@ -12,52 +12,35 @@ driver = webdriver.Chrome(chromedriver)
 driver.get("https://toster.ru/")
 questions = driver.find_elements_by_class_name("question_short")
 
+for element in questions:
+    print(element.find_element_by_class_name("question__title-link").text)
+    data_publish = element.find_element_by_class_name("question__date_publish").get_attribute("datetime").split()[0]
+    print("Date of public -", data_publish)
 
-"""
+    followers = element.find_element_by_class_name("question__views-count").text.split()[0]
+    if followers != "нет":
+        print("Number of followers -", followers)
+    else: print("Number of followers -", "0")
 
-for question in questions:
-    print(question.find_element_by_class_name("question__title-link"))
-"""
+    Number_answers = element.find_element_by_class_name("mini-counter__count_grey").text
+    print("Number of answers -", Number_answers)
 
-"""
-for question in questions:
-    print(question.find_element_by_class_name("question__title-link").text)
-"""
-"""
-# number of viewers
-for question in questions:
-    subscribers = question.find_element_by_class_name("question__views-count").text.split()[0]
-    print(subscribers)
-"""
-"""
-# date of public
-for question in questions:
-    date_public = question.find_element_by_class_name("question__date_publish").get_attribute("datetime")
-    print(date_public)
-"""
-"""
-# check icon image
-for question in questions:
+# icon check
+
+
     try:
-        print(question.find_element_by_class_name("icon_check"))
-        is_answered = True
-    except NoSuchElementExeption:
-        is_answered = False
-    print(is_answered)
-"""
-"""
-# going to pages
-for question in questions:
-    try:
-        print(question.find_element_by_css_selector("paginator__item"))
-        is_pages = True
-    except NoSuchElementExeption:
-        is_pages = False
-    print(is_pages)
-"""
+        element.find_element_by_class_name("icon_check")
+        correct_answer = True
+    except NoSuchElementException:
+        correct_answer = False
+    print("Correct answers -",correct_answer)
 
-# size window
-driver.set_window_size(414, 736)
+    paginators = driver.find_elements_by_class_name("paginator").text.split()
 
+    print(paginators)
+
+
+    # size window
+    driver.set_window_size(414, 736)
 
 driver.quit()
