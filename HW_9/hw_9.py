599245f8c4c735b785216f089eb31d990be277ea
Dmitriy Protsiuk <d.Protsiuk@gmail.com>
2017-03-27 01:18:36 +0300

    Add coll function vithout voice to Animal

--- HW_9/hw_9.py
+++ HW_9/hw_9.py
@@ -1,31 +1,44 @@
 # homework 9
 
 class Animal(object):
-
     def __init__(self, weight, is_big, speed):
         self.weight = weight
         self.is_big = is_big
         self.speed = speed
 
     def impuls_power(self):
-        return int(self.weight.split()[0])*int(self.speed.split()[0]), "kg*m/s"
+        power = int(self.weight.split()[0]) * int(self.speed.split()[0])
+        return power, "kg*m/s"
+
 
 class CatFamily(Animal):
     def voice(self):
         return "This is animal - growls"
 
+
 class DogFamily(Animal):
     def voice(self):
         return "This is animal - barks"
 
-Cat = CatFamily("10 kg", "small", "30 km/h")
-Dog = DogFamily("30 kg", "middle", "30 km/h")
-Tiger = CatFamily("170 kg", "big", "80 km/h")
-Wolf = DogFamily("80 kg", "big", "65 km/h")
-Fox = DogFamily("10 kg", "small", "50 km/h")
-Hare = Animal("6 kg", "small", "40 km/h")
 
-print("Dog weight-", Dog.weight)
-print("Tiger power -",Tiger.impuls_power())
-print("Hare speed -", Hare.speed)
+class Rabbit(Animal):
+    pass
 
+
+cat = CatFamily("10 kg", "small", "30 km/h")
+dog = DogFamily("30 kg", "middle", "30 km/h")
+tiger = CatFamily("170 kg", "big", "80 km/h")
+wolf = DogFamily("80 kg", "big", "65 km/h")
+fox = DogFamily("10 kg", "small", "50 km/h")
+hare = Rabbit("6 kg", "small", "40 km/h")
+
+
+def request_voice(ClassOdj):
+    try:
+        return ClassOdj.voice()
+    except AttributeError:
+        return "This is animal has not voice!"
+
+print(request_voice(fox))
+print(request_voice(cat))
+print(request_voice(hare))
